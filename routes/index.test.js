b269cb5b8c4a99e22b4a1209c73452c8d5191230
Dylan On <dylanon@users.noreply.github.com>
2018-02-05 22:34:49 -0500

    Add unit tests for /write and /read/:id routes

--- routes/index.test.js
+++ routes/index.test.js
@@ -1,10 +1,22 @@
 const request = require('supertest');
 const express = require('express');
+const MongoClient = require('mongodb');
 
 const app = express();
+const env = require('../env');
 const defineRoutes = require('./index');
 
-defineRoutes(app, null);
+beforeAll(() => {
+    let collection;
+    return MongoClient.connect(env.MONGODB_URI)
+    .then(client => {
+        collection = client.db(env.MONGODB_NAME).collection('drops');
+        defineRoutes(app, collection);
+    })
+    .catch(error => {
+        console.log('Could not connect to MongoDB. ' + error.message);
+    });
+});
 
 it('should respond with "Rooting for you!"', () => {
     return request(app)
@@ -12,4 +24,86 @@ it('should respond with "Rooting for you!"', () => {
     .then(res => {
         expect(res.text).toBe('Rooting for you!');
     });
+});
+
+describe('test find and delete of a document', () => {
+    let documentId;
+    beforeAll(() => {
+        return request(app)
+        .post('/write')
+        // Send 'as application/x-www-form-urlencoded'
+        .type('form')
+        .send({
+            message: 'A test message'
+        })
+        .then(res => {
+            documentId = res.body.id;
+        })
+        .catch(err => {
+            console.log('Setup failed.');
+            console.log(err);
+        });
+    });
+
+    it('should respond with a valid id when a document is accessed', () => {
+        return request(app)
+        .get(`/read/${documentId}`)
+        .then(res => {
+            expect(res.body.deletedId).toBe(documentId);
+        })
+    });
+
+    it('should respond with null data when the requested id is too short', () => {
+        return request(app)
+        .get('/read/1')
+        .then(res => {
+            expect(res.body.deletedId).toBe(null);
+            expect(res.body.message).toBe(null);
+        })
+    });
+    
+    it('should respond with null data when the requested id is too long', () => {
+        return request(app)
+        .get('/read/123456789123456789123456789')
+        .then(res => {
+            expect(res.body.deletedId).toBe(null);
+            expect(res.body.message).toBe(null);
+        })
+    });
+    
+    it('should respond with null data when the requested id is not found', () => {
+        return request(app)
+        .get('/read/111111111111111111111111')
+        .then(res => {
+            expect(res.body.deletedId).toBe(null);
+            expect(res.body.message).toBe(null);
+        });
+    });
+});
+
+it('should say that write succeeded and return a valid id when a proper message is POSTed', () => {
+    return request(app)
+    .post('/write')
+    // Send 'as application/x-www-form-urlencoded'
+    .type('form')
+    .send({
+        message: 'A test message'
+    })
+    .then(res => {
+        expect(res.body.writeSucceeded).toBe(true);
+        expect(res.body.id.length).toBe(24);
+    });
+});
+
+it('should say that write did not succeed when the message property is missing from a POST', () => {
+    return request(app)
+    .post('/write')
+    .type('form')
+    .send({
+        notMessage: 'A test message'
+    })
+    .then(res => {
+        expect(res.body.writeSucceeded).toBe(false);
+        expect(res.body.id).toBe(undefined);
+    })
 });
