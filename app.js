ebcfd6c2c7d6d985d1c566343b789ee4daefd75e
Dylan On <dylanon@users.noreply.github.com>
2018-02-05 23:33:26 -0500

    Rename index.js to app.js, replace MongoDB connection callback with promises

--- app.js
+++ app.js
@@ -13,14 +13,11 @@ const app = express();
 const MONGODB_URI = process.env.MONGODB_URI || env.MONGODB_URI;
 const MONGODB_NAME = process.env.MONGODB_NAME || env.MONGODB_NAME;
 
-MongoClient.connect(MONGODB_URI, (error, client) => {
-    if (error) {
-        return console.log(error);
-    }
+MongoClient.connect(MONGODB_URI)
+.then(client => {
     console.log('Connected to MongoDB');
     const db = client.db(MONGODB_NAME);
     const collection = db.collection('drops');
-
     // Enable CORS
     // (See https://enable-cors.org/server_expressjs.html)
     app.use(function(req, res, next) {
@@ -28,8 +25,11 @@ MongoClient.connect(MONGODB_URI, (error, client) => {
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
     });
-
+    
     defineRoutes(app, collection);
+})
+.catch(error => {
+    console.log(error);
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('./index.js');
+const app = require('./app');
 const env = require('./env');
 
 // Set port
